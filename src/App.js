90d94430bfd5a6c9ee42f361254ac53b48f1b641
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-05-03 21:56:24 +0200

    🚧 springy

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import Card from './Card'
 import './index.scss';
 import './card.scss';
 
-// import ltfImg from './imgs/ltf.jpg'
-import ltfImg from './imgs/img.webp'
+import ltfImg from './imgs/ltf.jpg'
+// import ltfImg from './imgs/img.webp'
 
 
 
@@ -44,24 +44,24 @@ const cardData = [
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quos excepturi? Temporibus aspernatur a et quasi at quos',
     color: '#ccc',
   },
-  {
-    title: 'Bike Repair',
-    image: ltfImg,
-    description: 'Lorem ipibus, saepe tempore, veniam neque iusto aperiam! Id!',
-    color: 'blue',
-  },
-  {
-    title: 'RealAs',
-    image: ltfImg,
-    description: 'Lorem ipsum dolor sit, am',
-    color: '#fff',
-  },
-  {
-    title: 'Tekentool',
-    image: ltfImg,
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quos excepturi? Temporibus aspernatur a',
-    color: 'green'
-  },
+  // {
+  //   title: 'Bike Repair',
+  //   image: ltfImg,
+  //   description: 'Lorem ipibus, saepe tempore, veniam neque iusto aperiam! Id!',
+  //   color: 'blue',
+  // },
+  // {
+  //   title: 'RealAs',
+  //   image: ltfImg,
+  //   description: 'Lorem ipsum dolor sit, am',
+  //   color: '#fff',
+  // },
+  // {
+  //   title: 'Tekentool',
+  //   image: ltfImg,
+  //   description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quos excepturi? Temporibus aspernatur a',
+  //   color: 'green'
+  // },
 ]
 
 const App = () => {
--- src/Card.js
+++ src/Card.js
@@ -8,6 +8,8 @@ const padding = 16
 
 const tension = 400
 
+const config = { tension: 200, friction: 30, mass: 2.2 }
+
 const Card = ({
   isActive,
   item,
@@ -15,42 +17,57 @@ const Card = ({
   handleActivate,
   draggerX,
 }) => {
-  
-  // backdrop
-  const refBackdrop = useRef(null);
-  const { width: backdropWidth, height: backdropHeight, x: backdropX, y: backdropY } = useElementResizer(refBackdrop)
-  const backdropCenterX = (window.innerWidth / 2) - (backdropWidth / 2) - backdropX - draggerX - padding
-  const backdropCenterY = (window.innerHeight / 2) - (backdropHeight / 2) - backdropY //+ hoverY
-  const backdropScreenX = window.innerWidth / backdropWidth
-  const backdropOff = 'translate3d(0px, 0px, 0px) scale(1, 1)'
-  const backdropOn = `translate3d(${backdropCenterX}px, ${backdropCenterY}px, 0px) scale(${backdropScreenX}, ${backdropScreenX})`
-  const { transformBackdrop } = useSpring({
-    transformBackdrop: isActive ? backdropOn : backdropOff,
-    config: { mass: 0.5, tension: tension*2, friction: 40 }
-  })
-  const transformerRefOffsetLeft = useElementResizer(refBackdrop).left
-  const parallaxVal = (draggerX + transformerRefOffsetLeft) / parallaxFactor
 
   // image
   const refImage = useRef(null);
   const { width: imageWidth, height: imageHeight, x: imageX, y: imageY } = useElementResizer(refImage)
+
+  
+  // backdrop
+  const refBackdrop = useRef(null);
+  const { width: backdropWidth, height: backdropHeight, x: backdropX, y: backdropY } = useElementResizer(refBackdrop)
+  
   const imageCenterX = -imageX + (window.innerWidth / 2) - (imageWidth / 2) - draggerX - padding
+  const imageCenterY = (window.innerHeight / 2) - (imageHeight / 2) - imageY
+  
+  console.log(imageY, imageHeight, backdropY, backdropHeight)
+
+  const backdropCenterX = (window.innerWidth / 2) - (backdropWidth / 2) - backdropX - draggerX - padding
+  const backdropCenterY = (window.innerHeight / 2) - (backdropHeight / 2) - backdropY - imageY//+ hoverY
+  const backdropScaleX = window.innerWidth / backdropWidth
+  const backdropScaleY = window.innerHeight / backdropHeight
+
+  
+  const backdropOff = 'translate3d(0px, 0px, 0px) scale(1)'
+  const backdropOn = `translate3d(${backdropCenterX}px, ${-imageY - backdropY}px, 0px) scale(${backdropScaleX })` //backdropScaleX
+  const { transformBackdrop } = useSpring({
+    transformBackdrop: isActive ? backdropOn : backdropOff,
+    config
+  })
+  const transformerRefOffsetLeft = useElementResizer(refBackdrop).left
+  const parallaxVal = (draggerX + transformerRefOffsetLeft) / parallaxFactor
+
+  
+  
   // const imageCenterX = -imageX + (imageWidth / 2) - (window.innerWidth /2 )// - draggerX - padding
-  const imageCenterY = (window.innerHeight / 2) - (imageHeight / 2 ) - imageY
+  
   // const imageScreenX = window.innerWidth / imageWidth
   // const imageScreenY =  window.innerHeight / imageHeight
 
-  const imageOff = `translate3d(${parallaxVal}px, 0px, 0px) scale(2, 2)`
-  const imageOn = `translate3d(${imageCenterX }px, ${imageCenterY}px, 0px) scale(${backdropScreenX}, ${backdropScreenX})`
+  const imageOff = `translate3d(${0}px, 0px, 0px) scale(1.5)` // 1.5
+  const imageOn = `translate3d(${0}px, 0px, 0px) scale(1)` // 1.25
+  // const imageOn = `translate3d(${imageCenterX }px, ${imageCenterY}px, 0px) scale(${backdropScaleX}, ${backdropScaleX})`
   const { transformImage } = useSpring({
     transformImage: isActive ? imageOn : imageOff,
-    config: { mass: 3.5, tension: tension*0.5, friction: 40 }
+    config
   })
 
-  const { clipPath } = useSpring({
-    clipPath: isActive ? `inset(${-175}% ${-165}% ${-165}% ${-195}% round 0 0 0 0)` : `inset(${0}% ${0}% ${0}% ${0}% round 0 0 12px 12px)`,
-    config: { tension: 210, friction: 20 }
-  })
+  // console.log(imageCenterY, backdropCenterY, imageY, backdropY)
+
+  // const { clipPath } = useSpring({
+  //   clipPath: isActive ? `inset(${-window.innerHeight/2}px ${-165}px ${-165}px ${-195}px round 0 0 0 0)` : `inset(${0}px ${0}px ${0}px ${0}px round 0 0 12px 12px)`,
+  //   config: { mass: 0.5, tension: tension * 0.5, friction: 40 }
+  // })
 
 
   return (
@@ -60,7 +77,8 @@ const Card = ({
       onClick={() => handleActivate(i)}
       href="/"
     >
-    
+
+
       <animated.div
         className="item_content"
         style={{
@@ -70,38 +88,39 @@ const Card = ({
         <h2 className="item_title">{item.title}</h2>
         <p>{item.description}</p>
       </animated.div>
-
-      <animated.div
-        className="clipper"
-        style={{ 
-          clipPath: clipPath.interpolate(t => t),
-          // overflow: isActive ? 'visible' : 'hidden',
-        }}
-    >
-
-        <animated.img
-          className="item_media"
-          src={item.image}
-          alt=""
-          ref={refImage}
-          style={{
-            transform: transformImage.interpolate(t => t),
-            // transformOrigin: isActive ? 'inherit' : 'bottom'
-          }}
-        // style={{
-        //   transform: isActive ? `translate(${(backdropCenterX) - getTranslateX(this.mediaRefs[i])}px, ${backdropCenterY / 2}px) scale(5)` : 'scale(1.5)',
-        //   filter: isActive ? 'blur(3px)' : 'none',
-        // }}
-        />
-
-      </animated.div>
-
-
+      
       <animated.div
         ref={refBackdrop}
         className="backdrop"
-        style={{ transform: transformBackdrop.interpolate(t => t) }}
-      />
+        style={{ 
+          transform: transformBackdrop.interpolate(t => t),
+          // clipPath: clipPath.interpolate(t => t),
+        }}
+      >
+        {/* <animated.div
+          className="clipper"
+          style={{
+            clipPath: clipPath.interpolate(t => t),
+            // overflow: isActive ? 'visible' : 'hidden',
+          }}
+        > */}
+
+          <animated.img
+            className="item_media"
+            src={item.image}
+            alt=""
+            ref={refImage}
+            style={{
+              transform: transformImage.interpolate(t => t),
+            }}
+          // style={{
+          //   transform: isActive ? `translate(${(backdropCenterX) - getTranslateX(this.mediaRefs[i])}px, ${backdropCenterY / 2}px) scale(5)` : 'scale(1.5)',
+          //   filter: isActive ? 'blur(3px)' : 'none',
+          // }}
+          />
+
+        {/* </animated.div> */}
+      </animated.div>
     </button>
   )
 }
