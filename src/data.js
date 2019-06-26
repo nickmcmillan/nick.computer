00bdcbf252996ef7cae797d62730362832eb66b2
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-06-26 16:00:38 +0200

    getting there

--- src/data.js
+++ src/data.js
@@ -20,14 +20,19 @@ import { ReactComponent as ReduxIcon } from './icons/redux.svg'
 import { ReactComponent as Google } from './icons/google.svg'
 import { ReactComponent as ReactSpring } from './icons/react-spring.svg'
 import { ReactComponent as Cloudinary } from './icons/cloudinary.svg'
+import { ReactComponent as Blender } from './icons/blender.svg'
 
 export default [
   {
     title: 'Fenton',
     path: '/fenton',
-    offsetY: 100,
+    offsetY: 80,
     logo: fentonSvg,
     icons: [
+      {
+        Icon: Blender,
+        title: 'Blender'
+      },
       {
         Icon: ThreeIcon,
         title: 'three.js'
@@ -38,7 +43,7 @@ export default [
     imageSm: fentonSm,
     intro: 'A three.js website for the Melbourne band Fenton',
     subtitle: 'A three.js website for the Melbourne band Fenton',
-    description: '<p>Built as a playful online presence for the band which I\'m a part of.</p><p>In terms of the code it uses a combination of <a href="#">three.js</a> and <a href="#">cannon.js</a> which provides the physics. The models used were found online but were tweaked and cleaned up using Blender to make them download as fast as possible.</p><p>It works great on mobile and desktop, and on larger screens you can play around with a few of the camera and physics settings for fun.</p><p>Made in 2019. The source code is available on <a href="https://github.com/nickmcmillan/fenton-three-cannon" target="_blank">Github</a>.</p>',
+    description: '<p>Built as a playful online presence for the band which I\'m a part of.</p><p>I\'ve used the popular <a href="https://threejs.org/" rel="noopener nofollow" target="_blank">three.js</a> and combined it with <a href="https://schteppe.github.io/cannon.js/" rel="noopener nofollow" target="_blank">cannon.js</a> which provides the physics. I downloaded most of the models from online marketplaces and cleaned them up using Blender - simplifying model complexity greatly reduces filesize.</p><p>It works great on mobile and desktop, and on larger screens you can play around with a few of the camera and physics settings for fun.</p><p>Made in 2019. The source code is available on <a href="https://github.com/nickmcmillan/fenton-three-cannon" rel="noopener nofollow" target="_blank">Github</a>.</p>',
     theme: '#f4f592',
   },
   {
@@ -72,7 +77,7 @@ export default [
   {
     title: 'mcfrench.co',
     path: '/mcfrench',
-    activeOpacity: 0.8,
+    activeOpacity: 0.9,
     offsetY: 80,
     icons: [
       {
@@ -93,7 +98,7 @@ export default [
     imageSm: mcfrenchSm,
     intro: 'A photo collection of our time spent living in Amsterdam',
     subtitle: 'A photo collection of our time spent living in Amsterdam',
-    description: '<p>I wanted to document and share the 1.5 year period where my partner and I lived in my second favourite city, Amsterdam (Melbourne being my number one).</p><p>The collection became enormous. So my challenge was to find a way to organise and layout the whole collection, all in a single page, without the browser choking on 3000+ images.</p><p>Inspired by <a href="https://medium.com/google-design/google-photos-45b714dfbed1">Antin Harasymiv\'s write-up</a> of how Google Photos does it, I forked <a href="https://github.com/schlosser/pig.js/">previous work</a> and extended it into an open-source module for React called <a href="https://github.com/nickmcmillan/react-pig">React Pig</a>.</p><p>I wrote Node functions which bulk upload images to Cloudinary. I chose Cloudinary as they support on-the-fly image resizing and have a well documented API.</p><p>React Pig works great. It can render thousands of images, creating a seemless scrolling experience with lazy-loading, clickable thumbnails, and can even group images by dates</p>',
+    description: '<p>I wanted to document and share the 1.5 year period where my partner and I lived in my second favourite city, Amsterdam (Melbourne being my number one).</p><p>The collection became enormous. So my challenge was to find a way to organise and layout the whole collection, all in a single page, without the browser choking on 3000+ images.</p><p>Inspired by <a href="https://medium.com/google-design/google-photos-45b714dfbed1">Antin Harasymiv\'s write-up</a> of how Google Photos does it, I forked <a href="https://github.com/schlosser/pig.js/" rel="noopener nofollow" target="_blank">previous work</a> and extended it into an open-source module for React called <a href="https://github.com/nickmcmillan/react-pig" rel="noopener nofollow" target="_blank">React Pig</a>.</p><p>I wrote Node functions which bulk upload images to Cloudinary. I chose Cloudinary as they support on-the-fly image resizing and have a well documented API.</p><p>React Pig works great. It can render thousands of images, creating a seemless scrolling experience with lazy-loading, clickable thumbnails, and can even group images by dates</p>',
     theme: '#dfecfb',
   },
   {
@@ -130,7 +135,7 @@ export default [
     imageSm: brSm,
     intro: 'A curated search tool for specialised bike shops.',
     subtitle: 'Hand-picked repair services that keep you riding',
-    description: '<p><a href="https://www.bikerepair.com/" target="_blank">Bikerepair.com</a> is a curated search tool for specialised bike shops.</p><p>I built the front-end using React + Redux for the UI and state management, <a href="https://github.com/google-map-react/google-map-react" target="_blank">Google Maps React</a> for the map, and <a href="https://github.com/react-spring/react-spring" target="_blank">react-spring</a> for the transitions and animations throughout the site.</p><p>Craft CMS runs on the backend to provide all the bike shop data which is then combined with ratings and customer reviews from Google Places API.</p><p>Built whilst at Momkai in 2018.</p>',
+    description: '<p><a href="https://www.bikerepair.com/" target="_blank">Bikerepair.com</a> is a curated search tool for specialised bike shops.</p><p>I built the front-end using React + Redux for the UI and state management, <a href="https://github.com/google-map-react/google-map-react" rel="noopener nofollow" target="_blank">Google Maps React</a> for the map, and <a href="https://github.com/react-spring/react-spring" rel="noopener nofollow" target="_blank">react-spring</a> for the transitions and animations throughout the site.</p><p>Craft CMS runs on the backend to provide all the bike shop data which is then combined with ratings and customer reviews from Google Places API.</p><p>Built whilst at Momkai in 2018.</p>',
     theme: '#1a8acc',
     textColor: '#fff'
   },
