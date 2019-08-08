50bb9f12f2a51d86c1eccd190e27ca988f63eeaa
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-08-08 14:08:24 +0200

    remove io on subheading

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,31 +1,16 @@
 import React, { useRef } from 'react'
-import { animated, useSpring } from 'react-spring'
-import useOnScreen from '../../Hooks/useOnScreen'
-import { configMain } from '../../App'
-
 import ListItem from '../ListItem/ListItem'
 
 const List = ({title, data, inert}) => {
 
   const ref = useRef()
-  const onScreen = useOnScreen(ref, '-100px')
-
-  const { x } = useSpring({
-    x: onScreen ? 0 : 100,
-    config: configMain
-  })
 
   return (
     <section className="section" inert={inert ? '' : undefined}>
       <div className="sub-heading-wrapper" ref={ref}>
-        <animated.h2
-          className="sub-heading"
-          style={{
-            transform: x.interpolate(x => `translate3d(0,${x}%,0)`),
-          }}
-        >
+        <h2 className="sub-heading">
           {title}
-        </animated.h2>
+        </h2>
       </div>
 
       <ul className="list">
