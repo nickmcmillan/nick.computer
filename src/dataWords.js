c7edda8423f84544fdf02c01200a58e04878579b
Nick McMillan <nick.a.mcmillan@gmail.com>
2020-05-05 14:51:19 +1000

    dates

--- src/dataWords.js
+++ src/dataWords.js
@@ -4,13 +4,13 @@ const dataOpenSource = [
   {
     href: 'https://medium.com/@nick.a.mcmillan/a-guide-to-adding-a-netlify-serverless-function-to-your-gatsby-site-34a85f4a5d2b',
     title: 'A guide to adding a Netlify serverless function to your Gatsby site',
-    // desc: 'A simple, no-frills horiztonal dragger/slider with physics.',
+    desc: 'Feb 22, 2020',
     Icon: MediumIcon,
   },
   {
     href: 'https://medium.com/@nick.a.mcmillan/adding-a-site-wide-search-to-a-gatsby-wordpress-project-84a65f40ab0f',
     title: 'Adding a site-wide search to a Gatsby Wordpress project',
-    // desc: 'A simple, no-frills horiztonal dragger/slider with physics.',
+    desc: 'May 3, 2020',
     Icon: MediumIcon,
   },
 ]
