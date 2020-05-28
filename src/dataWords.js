ebcc448e77fe0a48535fbb5b1c0c2cf0604e1c64
Nick McMillan <nick.a.mcmillan@gmail.com>
2020-05-28 13:41:42 +1000

    more words

--- src/dataWords.js
+++ src/dataWords.js
@@ -2,15 +2,21 @@ import { ReactComponent as MediumIcon } from './icons/medium.svg'
 
 const dataOpenSource = [
   {
-    href: 'https://medium.com/@nick.a.mcmillan/a-guide-to-adding-a-netlify-serverless-function-to-your-gatsby-site-34a85f4a5d2b',
-    title: 'A guide to adding a Netlify serverless function to your Gatsby site',
-    desc: 'Feb 22, 2020',
+    href: 'https://medium.com/mass-thinks/creating-the-mass-website-felt-like-i-was-getting-paid-to-have-fun-3ab4f4670035',
+    title: 'Creating the MASS website felt like I was getting paid to have fun.',
+    desc: 'May 27, 2020',
     Icon: MediumIcon,
   },
   {
     href: 'https://medium.com/@nick.a.mcmillan/adding-a-site-wide-search-to-a-gatsby-wordpress-project-84a65f40ab0f',
     title: 'Adding a site-wide search to a Gatsby Wordpress project',
     desc: 'May 3, 2020',
+    Icon: MediumIcon,
+  },
+  {
+    href: 'https://medium.com/@nick.a.mcmillan/a-guide-to-adding-a-netlify-serverless-function-to-your-gatsby-site-34a85f4a5d2b',
+    title: 'A guide to adding a Netlify serverless function to your Gatsby site',
+    desc: 'Feb 22, 2020',
     Icon: MediumIcon,
   },
 ]
